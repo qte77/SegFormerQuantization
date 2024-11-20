0cfcca270b7d74b9166ab2cd7324a581421696ed
qte77 <qte@77>
2024-11-20 23:56:26 +0100

    Fxe ghpages plugins

--- .github/scripts/gen_mkdocs_docstring_pages.py
+++ .github/scripts/gen_mkdocs_docstring_pages.py
@@ -7,8 +7,10 @@ code_index_file = environ["CODE_INDEX"]
 path_prefix = "docstrings"
 path_suffix = ".md"
 
+files_found = sorted(src_dir.glob("**/*.py"))
+
 # create docstrings md
-for path in src_dir.glob("**/*.py"):
+for path in files_found:
     if path.name != "__init__.py":
         doc_path = Path(path_prefix, path.relative_to(src_dir)).with_suffix(path_suffix)
         with mkdocs_gen_files.open(doc_path, "w") as f:
@@ -24,7 +26,7 @@ for path in src_dir.glob("**/*.py"):
 # append docstrings to navigation file
 with mkdocs_gen_files.open(code_index_file, "a") as nav_file:
     nav_file.write("# App Reference\n\n")
-    for path in sorted(src_dir.glob("**/*.py")):
+    for path in files_found:
         if path.name != "__init__.py":
             module_path = ".".join(path.with_suffix("").parts)
             doc_path = Path(path_prefix, path.relative_to(src_dir)).with_suffix(path_suffix)
